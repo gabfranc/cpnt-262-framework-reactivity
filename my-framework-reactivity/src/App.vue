8c24f47f9b459c38804251ae08c4dcf218fed26a
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-03-10 21:08:49 -0600

    add everything in vue

---
+++ my-framework-reactivity/src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+  import HelloWorld from './components/HelloWorld.vue';
+  import { ref } from 'vue'
+
+
+  const numCount = ref(0);
+  const message = "SHOW SONGS";
+  function increment() {
+    numCount.value++
+  }
+
+const showText = ref(false);
+
+function clickButton() {
+  showText.value = !showText.value;
+}
+</script>
+<template>
+  <main>
+    <section>
+     <h2>Song Names</h2>
+      <HelloWorld v-if="showText" />
+      <button @click="clickButton"><slot>{{ message }}</slot></button>
+
+    </section>
+
+
+  </main>
+  
+</template>
+
+<style scoped>
+  main {
+    display: flex;
+    flex-direction: row;
+  }
+
+  button {
+    cursor: pointer;
+    margin-top: 22px;
+    border: none;
+    height: 7vh;
+    width: 22vh;
+    background: #4274ad;
+    color: #bbcae9;
+    font-weight: 900;
+  }
+
+  
+</style>
---
+++ my-framework-reactivity/src/components/HelloWorld.vue
@@ -0,0 +1,62 @@
+<script setup>
+
+import '../assets/main.css'
+import { reactive } from "vue"
+
+const artist = reactive({
+  name: "Taylor Swift",
+  songs: [
+    "Our Song",
+    "Illicit Affairs",
+    "Cornelia Street",
+    "Paris",
+    "I Did Something Bad",
+    "Mean~ Taylor's Version",
+    "I Knew Places~Taylor's Version",
+    "Red~Taylor's Version"
+  ]
+})
+
+
+</script>
+
+<template>
+  <section>
+    <div>
+      <h4>{{ artist.name }}</h4>
+    </div>
+      <ul>
+        <li v-for="(song, index) in artist.songs" :key="index">
+          <span v-bind:class="{active: song.isActive}">
+            <a href="https://open.spotify.com/artist/06HL4z0CvFAxyc27GXpf02?si=aVPzFAH0Txmybdcb5qgJvw">{{ song }}</a>
+          </span>
+        </li>
+      </ul>
+    
+   </section>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
